67d3c25faf05b2bc4230e0e64417daf0a72613de
Nacho Valera Herrera <nachovaleraherreraes@gmail.com>
2016-11-07 02:10:22 +0100

    You can search products and they are paginated

--- utils/filters.inc.php
+++ utils/filters.inc.php
@@ -7,3 +7,10 @@ function filter_num_int($num) {
     return $return = array('resultado' => true, 'error' => "", 'datos' => $num);
 }
 
+function filter_string($cad){
+    $result = filter_var($cad, FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => '/^[a-zA-Z0-9 .,]*$/')));
+    if (!$result) {
+        return $return=array('resultado'=>false, 'error'=>"Invalid value input", 'datos'=>"");
+    }
+    return $return=array('resultado'=>true, 'error'=>"", 'datos'=>$cad);
+}
